64f2a0fff92231bbf8d4662c96f9d53a263f8a0c
Unimusha <Alextopota528@gmail.com>
2019-12-03 17:38:57 +0100

    Ejercicio IV - Parte 1

---
+++ database/migrations/2019_12_03_163306_add_nombre_y_apellidos_to_users.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddNombreYApellidosToUsers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('nombre')->after('remember_token');
+            $table->string('apellidos')->after('remember_token');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('nombre');
+            $table->dropColumn('apellidos');
+        });
+    }
+}
